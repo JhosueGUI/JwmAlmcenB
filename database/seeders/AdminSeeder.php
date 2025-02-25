033502956304e450b6eab8052feac12d5bc218e5
JhosueGUI <159204953+JhosueGUI@users.noreply.github.com.>
2025-02-25 12:45:19 -0500

    hola

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\Acceso;
@@ -9,37 +8,37 @@ use App\Models\Persona;
 use App\Models\Personal;
 use App\Models\User;
 use App\Models\UsuarioRol;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        $persona=Persona::firstOrcreate([
-            "nombre"=>"admin",
-            "tipo_documento_id"=>1,
-            "numero_documento"=>12082024
-        ]);
-        $personal=Personal::firstOrcreate([
-            "persona_id"=>$persona->id,
-            "area_id"=>1
-        ]);
-        $user=User::firstOrcreate([
-            "personal_id"=>$personal->id,
-            "username"=>"SuperAdmin",
-            "password"=>$persona->numero_documento
-        ]);
-        $user_rol=UsuarioRol::firstOrcreate([
-            "rol_id"=>1,
-            "user_id"=>$user->id
-        ]);
-        $accesos=Acceso::where('estado_registro','A')->get();
-        foreach($accesos as $acceso){
-            AccesoRol::firstOrcreate([
-                "rol_id"=>1,
-                "acceso_id"=>$acceso->id
-            ]);
+        $persona = Persona::updateOrCreate(
+            ["numero_documento" => 12082024], // Condición de búsqueda
+            ["nombre" => "admin", "tipo_documento_id" => 1] // Datos a actualizar si ya existe
+        );
+
+        $personal = Personal::updateOrCreate(
+            ["persona_id" => $persona->id], 
+            ["area_id" => 1]
+        );
+
+        $user = User::updateOrCreate(
+            ["username" => "SuperAdmin"],
+            ["personal_id" => $personal->id, "password" => bcrypt($persona->numero_documento)]
+        );
+
+        UsuarioRol::updateOrCreate(
+            ["rol_id" => 1, "user_id" => $user->id]
+        );
+
+        $accesos = Acceso::where('estado_registro', 'A')->get();
+        foreach ($accesos as $acceso) {
+            AccesoRol::updateOrCreate(
+                ["rol_id" => 1, "acceso_id" => $acceso->id]
+            );
         }
     }
 }
+
