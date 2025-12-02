6f43d96e25a356489ab1c01c3c88b216e9ad0ef3
JhosueGUI <1458823@gmail.com>
2025-12-01 23:38:50 -0500

    s

--- app/Exports/SalidaExport.php
+++ app/Exports/SalidaExport.php
@@ -11,10 +11,16 @@ class SalidaExport implements FromView
     /**
     * @return View
     */
-    public function view():View
-    {
-        return view('excel.SalidaExport', [
-            'Salidas' => Salida::all(),
-        ]);
-    }
+    public function view(): View
+{
+    return view('excel.SalidaExport', [
+        'Salidas' => Salida::with([
+            'personal.persona',
+            'transaccion.producto.articulo.sub_familia.familia',
+            'transaccion.producto.unidad_medida',
+            'transaccion.producto.inventario'
+        ])->get(),
+    ]);
+}
+
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,20 +6,14 @@ use Closure;
 
 class Cors
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:5173');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->header('Access-Control-Expose-Headers', 'Content-Disposition');
 
         return $response;
     }
