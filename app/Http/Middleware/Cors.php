964cdcb4b7575a6a9105619441394810df2d65c6
JhosueGUI <1458823@gmail.com>
2025-12-01 23:49:13 -0500

    s

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,13 +8,19 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+        // Si es un preflight OPTIONS request, respondemos inmediatamente
+        if ($request->getMethod() === "OPTIONS") {
+            return response('')
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', 'http://localhost:5173');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        $response->header('Access-Control-Expose-Headers', 'Content-Disposition');
-
-        return $response;
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
